57e522b14f97419d79e615eb6028ceb5581be9b1
[Laiba Irshd] <[laibairshad3007@gmail.com]>
2024-11-24 11:01:45 -0800

    Update database logic to optimize query performance

--- db.py
+++ db.py
@@ -22,7 +22,8 @@ def create_users_table():
             id SERIAL PRIMARY KEY,
             username VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP);
+            created_at TIMESTAMP DEFAULT NOW()
+            );
 
             """
         )
